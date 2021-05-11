aa005a2ad04b4b0b5e680312f80830307a90d233
Bill Sourour <bill.sourour@arcnovus.com>
2021-05-11 17:40:21 -0400

    formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import legacy from "@vitejs/plugin-legacy";
+
 // TODO: Investigate vite-plugin-vue-i18n
 // import vueI18n from "@intlify/vite-plugin-vue-i18n";
 // https://vitejs.dev/config/
