a09080bbd78a7c00132682413c7e4db8c58fa457
Bill Sourour <bill.sourour@arcnovus.com>
2021-05-10 21:51:30 -0400

    ie alert

--- public/lang-fix.js
+++ public/lang-fix.js
@@ -4,13 +4,19 @@
 // in some of its widgets (e.g. the social media Share button and popup)
 // since the WET scripts need to run before any VueJS stuff, we need to patch
 // the language with a plain JS script called from index.html.
-// NB: This script needs to support IE11, so don't use any modern ES6+ syntax.
-// When in doubt consult caniuse.com.
 
 (function () {
   var DEFAULT_LANGUAGE = "en";
   var SUPPORTED_LANGUAGES = ["en", "fr"];
   var LANG_KEY = "lang";
+
+  var ie =
+    navigator.userAgent.indexOf("MSIE ") > -1 ||
+    navigator.userAgent.indexOf("Trident/") > -1;
+
+  if (ie) {
+    alert("This site does not support Internet Explorer.");
+  }
 
   setCurrentLanguage(SUPPORTED_LANGUAGES, DEFAULT_LANGUAGE, LANG_KEY);
 
--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     vue(),
     legacy({
       targets: ["defaults or IE > 10"],
+      additionalLegacyPolyfills: [],
     }),
   ],
 });
